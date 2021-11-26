4f58e539c52332281734b76808e8a7081c4fa713
Sweta0118 <90339061+Sweta0118@users.noreply.github.com>
2021-11-26 17:31:45 +0530

    Create Person.js

---
+++ Person.js
@@ -0,0 +1,9 @@
+var Person = new Object();
+Person.firstName = "SWETA";
+Person.lastName = "KUMARI"
+Person.address = new Object();
+Person.address.city = "jamui"
+Person.address.district ="jamui"
+Person.address.state = "Bihar"
+Person.address.PIN = "123456"
+Console.log(Person)
